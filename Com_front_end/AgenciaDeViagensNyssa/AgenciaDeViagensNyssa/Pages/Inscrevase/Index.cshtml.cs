9d2a8e119a70b2a9c8762058435eccad53733b27
Renato Wéssmėr <rewesa@outlook.com.br>
2022-11-28 05:55:17 -0300

    Create Index.cshtml.cs

---
+++ Com_front_end/AgenciaDeViagensNyssa/AgenciaDeViagensNyssa/Pages/Inscrevase/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AgenciaDeViagensNyssa.Pages.Inscrevase
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
